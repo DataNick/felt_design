6db2335dc8e91ae541a7a12260dd5dc0a2f51604
DataNick <DataNick@users.noreply.github.com>
2016-05-01 00:24:23 -0400

    Added task to compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,35 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    rubysass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    pngquant = require('imagemin-pngquant'),
+    del = require('del');
 
 
- gulp.task('default', function(){
-   // Default task code
- });
+gulp.task('sass', function(){
+  gulp.src('./source/stylesheets/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./source/stylesheets'));
+})
+
+gulp.task('watch', function(){
+  livereload.listen();
+
+  gulp.watch('./source/stylesheets/**/*.scss', ['sass']);
+  gulp.watch(['./source/stylesheets/style.css'], function(files){
+    livereload.changed(files)
+  });
+});
